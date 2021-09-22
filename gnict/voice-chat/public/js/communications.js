6ce206722188c18bd9d6f18704e40eb8085f8f1d
Hyunduk Shin <hyunduk0206@gmail.com>
2021-09-22 19:05:09 +0900

    Update webrtc.js

--- gnict/voice-chat/public/js/communications.js
+++ gnict/voice-chat/public/js/communications.js
@@ -1,258 +1,27 @@
 import { settingsEndpoint, offerEndpoint, debugMode } from "./config/config.js";
-import { sdpFilterCodec } from "./libs/webrtc.js";
+import { webrtcStart, webrtcStop } from "./libs/webrtc.js";
 
 // get DOM elements
-const startBtn = document.getElementById("start"),
-  stopBtn = document.getElementById("stop");
-
-const dataChannelLog = document.getElementById("data-channel"),
-  iceConnectionLog = document.getElementById("ice-connection-state"),
-  iceGatheringLog = document.getElementById("ice-gathering-state"),
-  signalingLog = document.getElementById("signaling-state");
-
-// webrtc params
-let webrtcParams;
+const stopBtn = document.getElementById("stop");
 
 // peer connection
 let pc;
 
 // data channel
-let dc, dcInterval;
-
-function createPeerConnection() {
-  let config = {
-    sdpSemantics: "unified-plan",
-  };
-
-  if (webrtcParams["use-stun"]) {
-    config.iceServers = [{ urls: ["stun:stun.l.google.com:19302"] }];
-  }
-
-  pc = new RTCPeerConnection(config);
-
-  // register some listeners to help debugging
-  pc.addEventListener(
-    "icegatheringstatechange",
-    function () {
-      iceGatheringLog.textContent += " -> " + pc.iceGatheringState;
-    },
-    false
-  );
-  iceGatheringLog.textContent = pc.iceGatheringState;
-
-  pc.addEventListener(
-    "iceconnectionstatechange",
-    function () {
-      iceConnectionLog.textContent += " -> " + pc.iceConnectionState;
-    },
-    false
-  );
-  iceConnectionLog.textContent = pc.iceConnectionState;
-
-  pc.addEventListener(
-    "signalingstatechange",
-    function () {
-      signalingLog.textContent += " -> " + pc.signalingState;
-    },
-    false
-  );
-  signalingLog.textContent = pc.signalingState;
-
-  // connect audio / video
-  pc.addEventListener("track", function (evt) {
-    if (evt.track.kind == "video" && debugMode) {
-      document.getElementById("webrtc_video").srcObject = evt.streams[0];
-      // console.log("video received! -------->", evt.streams);
-    } else {
-      document.getElementById("webrtc_audio").srcObject = evt.streams[0];
-      // console.log("Audio received! -------->", evt.streams);
-    }
-  });
-
-  return pc;
-}
-
-function negotiate() {
-  return pc
-    .createOffer()
-    .then(function (offer) {
-      return pc.setLocalDescription(offer);
-    })
-    .then(function () {
-      // wait for ICE gathering to complete
-      return new Promise(function (resolve) {
-        if (pc.iceGatheringState === "complete") {
-          resolve();
-        } else {
-          function checkState() {
-            if (pc.iceGatheringState === "complete") {
-              pc.removeEventListener("icegatheringstatechange", checkState);
-              resolve();
-            }
-          }
-          pc.addEventListener("icegatheringstatechange", checkState);
-        }
-      });
-    })
-    .then(function () {
-      let offer = pc.localDescription;
-      let codec;
-
-      codec = webrtcParams["audio-codec"];
-      if (codec !== "default") {
-        offer.sdp = sdpFilterCodec("audio", codec, offer.sdp);
-      }
-
-      codec = webrtcParams["video-codec"];
-      if (codec !== "default") {
-        offer.sdp = sdpFilterCodec("video", codec, offer.sdp);
-      }
-
-      if (debugMode) {
-        document.getElementById("offer-sdp").textContent = offer.sdp;
-      }
-
-      return fetch(offerEndpoint, {
-        body: JSON.stringify({
-          sdp: offer.sdp,
-          type: offer.type,
-          video_transform: webrtcParams["video-transform"],
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-        method: "POST",
-      });
-    })
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (answer) {
-      if (debugMode) {
-        document.getElementById("answer-sdp").textContent = answer.sdp;
-      }
-      return pc.setRemoteDescription(answer);
-    })
-    .catch(function (e) {
-      alert(e);
-    });
-}
-
-function start() {
-  document.getElementById("start").style.display = "none";
-
-  pc = createPeerConnection();
-
-  let time_start;
-
-  function current_stamp() {
-    if (time_start === undefined) {
-      time_start = new Date().getTime();
-      return 0;
-    } else {
-      return new Date().getTime() - time_start;
-    }
-  }
-
-  if (webrtcParams["use-datachannel"]) {
-    let parameters = JSON.parse(webrtcParams["datachannel-parameters"]);
-
-    dc = pc.createDataChannel("chat", parameters);
-    dc.onclose = function () {
-      clearInterval(dcInterval);
-      dataChannelLog.textContent += "- close\n";
-    };
-    dc.onopen = function () {
-      dataChannelLog.textContent += "- open\n";
-      dcInterval = setInterval(function () {
-        let message = "ping " + current_stamp();
-        dataChannelLog.textContent += "> " + message + "\n";
-        dc.send(message);
-      }, 1000);
-    };
-    dc.onmessage = function (evt) {
-      dataChannelLog.textContent += "< " + evt.data + "\n";
-
-      if (evt.data.substring(0, 4) === "pong") {
-        let elapsed_ms = current_stamp() - parseInt(evt.data.substring(5), 10);
-        dataChannelLog.textContent += " RTT " + elapsed_ms + " ms\n";
-      }
-    };
-  }
-
-  let constraints = {
-    audio: webrtcParams["use-audio"],
-    video: webrtcParams["use-video"],
-  };
-
-  if (webrtcParams["use-video"]) {
-    let videoResolution = webrtcParams["video-resolution"].split("x");
-    constraints.video = {
-      width: parseInt(videoResolution[0], 0),
-      height: parseInt(videoResolution[1], 0),
-    };
-  }
-
-  if (constraints.audio || constraints.video) {
-    if (constraints.video) {
-      document.getElementById("media").style.display = "block";
-    }
-    navigator.mediaDevices.getUserMedia(constraints).then(
-      function (stream) {
-        stream.getTracks().forEach(function (track) {
-          pc.addTrack(track, stream);
-          // console.log("-------------->", track);
-        });
-        return negotiate();
-      },
-      function (err) {
-        alert("Could not acquire media: " + err);
-      }
-    );
-  } else {
-    negotiate();
-  }
-
-  document.getElementById("stop").style.display = "inline-block";
-}
-
-function stop() {
-  document.getElementById("stop").style.display = "none";
-
-  // close data channel
-  if (dc) {
-    dc.close();
-  }
-
-  // close transceivers
-  if (pc.getTransceivers) {
-    pc.getTransceivers().forEach(function (transceiver) {
-      if (transceiver.stop) {
-        transceiver.stop();
-      }
-    });
-  }
-
-  // close local audio / video
-  pc.getSenders().forEach(function (sender) {
-    sender.track.stop();
-  });
-
-  // close peer connection
-  setTimeout(function () {
-    pc.close();
-  }, 500);
-}
+let dc;
 
 fetch(settingsEndpoint)
   .then((res) => res.json())
-  .then((data) => {
-    webrtcParams = data;
+  .then((webrtcParams) => {
+    ({ pc, dc } = webrtcStart(pc, dc, offerEndpoint, webrtcParams, debugMode));
+    document.getElementById("start").style.display = "none";
+    document.getElementById("stop").style.display = "inline-block";
   })
-  .then(start)
   .catch(function (e) {
     alert(e);
   });
 
-// startBtn.onclick = () => start();
-stopBtn.onclick = () => stop();
+stopBtn.onclick = () => {
+  webrtcStop(pc, dc);
+  stopBtn.style.display = "none";
+};
--- gnict/voice-chat/public/js/libs/webrtc.js
+++ gnict/voice-chat/public/js/libs/webrtc.js
@@ -1,3 +1,235 @@
+// public functions
+
+function webrtcStart(pc, dc, offerEndpoint, webrtcParams, debugMode) {
+  const dataChannelLog = document.getElementById("data-channel");
+  let dcInterval;
+
+  pc = createPeerConnection(webrtcParams["use-stun"], debugMode);
+
+  let time_start;
+
+  function current_stamp() {
+    if (time_start === undefined) {
+      time_start = new Date().getTime();
+      return 0;
+    } else {
+      return new Date().getTime() - time_start;
+    }
+  }
+
+  if (webrtcParams["use-datachannel"]) {
+    let parameters = JSON.parse(webrtcParams["datachannel-parameters"]);
+
+    dc = pc.createDataChannel("chat", parameters);
+    dc.onclose = function () {
+      clearInterval(dcInterval);
+      dataChannelLog.textContent += "- close\n";
+    };
+    dc.onopen = function () {
+      dataChannelLog.textContent += "- open\n";
+      dcInterval = setInterval(function () {
+        let message = "ping " + current_stamp();
+        dataChannelLog.textContent += "> " + message + "\n";
+        dc.send(message);
+      }, 1000);
+    };
+    dc.onmessage = function (evt) {
+      dataChannelLog.textContent += "< " + evt.data + "\n";
+
+      if (evt.data.substring(0, 4) === "pong") {
+        let elapsed_ms = current_stamp() - parseInt(evt.data.substring(5), 10);
+        dataChannelLog.textContent += " RTT " + elapsed_ms + " ms\n";
+      }
+    };
+  }
+
+  let constraints = {
+    audio: webrtcParams["use-audio"],
+    video: webrtcParams["use-video"],
+  };
+
+  if (webrtcParams["use-video"]) {
+    let videoResolution = webrtcParams["video-resolution"].split("x");
+    constraints.video = {
+      width: parseInt(videoResolution[0], 0),
+      height: parseInt(videoResolution[1], 0),
+    };
+  }
+
+  if (constraints.audio || constraints.video) {
+    if (constraints.video) {
+      document.getElementById("media").style.display = "block";
+    }
+    navigator.mediaDevices.getUserMedia(constraints).then(
+      function (stream) {
+        stream.getTracks().forEach(function (track) {
+          pc.addTrack(track, stream);
+          // console.log("-------------->", track);
+        });
+        return negotiate(pc, webrtcParams, offerEndpoint, debugMode);
+      },
+      function (err) {
+        alert("Could not acquire media: " + err);
+      }
+    );
+  } else {
+    negotiate(pc, webrtcParams, offerEndpoint, debugMode);
+  }
+
+  return { pc, dc };
+}
+
+function webrtcStop(pc, dc) {
+  // close data channel
+  if (dc) {
+    dc.close();
+  }
+
+  // close transceivers
+  if (pc.getTransceivers) {
+    pc.getTransceivers().forEach(function (transceiver) {
+      if (transceiver.stop) {
+        transceiver.stop();
+      }
+    });
+  }
+
+  // close local audio / video
+  pc.getSenders().forEach(function (sender) {
+    sender.track.stop();
+  });
+
+  // close peer connection
+  setTimeout(function () {
+    pc.close();
+  }, 500);
+}
+
+// private functions
+
+const createPeerConnection = (useStun, debugMode) => {
+  const iceConnectionLog = document.getElementById("ice-connection-state"),
+    iceGatheringLog = document.getElementById("ice-gathering-state"),
+    signalingLog = document.getElementById("signaling-state");
+
+  let config = {
+    sdpSemantics: "unified-plan",
+  };
+
+  if (useStun) {
+    config.iceServers = [{ urls: ["stun:stun.l.google.com:19302"] }];
+  }
+
+  const pc = new RTCPeerConnection(config);
+
+  // register some listeners to help debugging
+  pc.addEventListener(
+    "icegatheringstatechange",
+    function () {
+      iceGatheringLog.textContent += " -> " + pc.iceGatheringState;
+    },
+    false
+  );
+  iceGatheringLog.textContent = pc.iceGatheringState;
+
+  pc.addEventListener(
+    "iceconnectionstatechange",
+    function () {
+      iceConnectionLog.textContent += " -> " + pc.iceConnectionState;
+    },
+    false
+  );
+  iceConnectionLog.textContent = pc.iceConnectionState;
+
+  pc.addEventListener(
+    "signalingstatechange",
+    function () {
+      signalingLog.textContent += " -> " + pc.signalingState;
+    },
+    false
+  );
+  signalingLog.textContent = pc.signalingState;
+
+  // connect audio / video
+  pc.addEventListener("track", function (evt) {
+    if (evt.track.kind == "video" && debugMode) {
+      document.getElementById("webrtc_video").srcObject = evt.streams[0];
+      // console.log("video received! -------->", evt.streams);
+    } else {
+      document.getElementById("webrtc_audio").srcObject = evt.streams[0];
+      // console.log("Audio received! -------->", evt.streams);
+    }
+  });
+
+  return pc;
+};
+
+const negotiate = (pc, webrtcParams, offerEndpoint, debugMode) => {
+  return pc
+    .createOffer()
+    .then(function (offer) {
+      return pc.setLocalDescription(offer);
+    })
+    .then(function () {
+      // wait for ICE gathering to complete
+      return new Promise(function (resolve) {
+        if (pc.iceGatheringState === "complete") {
+          resolve();
+        } else {
+          function checkState() {
+            if (pc.iceGatheringState === "complete") {
+              pc.removeEventListener("icegatheringstatechange", checkState);
+              resolve();
+            }
+          }
+          pc.addEventListener("icegatheringstatechange", checkState);
+        }
+      });
+    })
+    .then(function () {
+      let offer = pc.localDescription;
+      let codec;
+
+      codec = webrtcParams["audio-codec"];
+      if (codec !== "default") {
+        offer.sdp = sdpFilterCodec("audio", codec, offer.sdp);
+      }
+
+      codec = webrtcParams["video-codec"];
+      if (codec !== "default") {
+        offer.sdp = sdpFilterCodec("video", codec, offer.sdp);
+      }
+
+      if (debugMode) {
+        document.getElementById("offer-sdp").textContent = offer.sdp;
+      }
+
+      return fetch(offerEndpoint, {
+        body: JSON.stringify({
+          sdp: offer.sdp,
+          type: offer.type,
+          video_transform: webrtcParams["video-transform"],
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+      });
+    })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (answer) {
+      if (debugMode) {
+        document.getElementById("answer-sdp").textContent = answer.sdp;
+      }
+      return pc.setRemoteDescription(answer);
+    })
+    .catch(function (e) {
+      alert(e);
+    });
+};
+
 const sdpFilterCodec = (kind, codec, realSdp) => {
   let allowed = [];
   let rtxRegex = new RegExp("a=fmtp:(\\d+) apt=(\\d+)\r$");
@@ -55,9 +287,8 @@ const sdpFilterCodec = (kind, codec, realSdp) => {
   return sdp;
 };
 
-// private functions
 const escapeRegExp = (string) => {
   return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"); // $& means the whole matched string
 };
 
-export { sdpFilterCodec };
+export { webrtcStart, webrtcStop };
